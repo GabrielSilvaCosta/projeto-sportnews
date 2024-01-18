1d7ae98ad0e665075d21b8b26f0a551f77533c00
GabrielSilvaCosta <bob@>
2024-01-18 19:38:08 -0300

    requisito 6 feito

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 # news/urls.py
 from django.urls import path
-from .views import home_page, news_details
+from .views import categories_form, home_page, news_details
 
 urlpatterns = [
     path('', home_page, name='home-page'),
     path('news/<int:id>/', news_details, name='news-details-page'),
+    path('categories/', categories_form, name='categories-form'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,7 @@
 # news/views.py
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+
+from news.forms import CategoryForm
 from .models import News
 
 
@@ -8,6 +10,18 @@ def home_page(request):
     return render(request, "home.html", {"news_list": news_list})
 
 
-def news_details(request, id):
-    news = get_object_or_404(News, id=id)
-    return render(request, 'news_details.html', {'news': news})
+def news_details(request, news_id):
+    news = get_object_or_404(News, pk=news_id)
+    return render(request, "news_details.html", {"news": news})
+
+
+def categories_form(request):
+    form = CategoryForm()
+
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home-page")
+
+    return render(request, "categories_form.html", {"form": form})
